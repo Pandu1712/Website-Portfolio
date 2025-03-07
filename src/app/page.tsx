409482855d659cb03ab6dcd7fff2cc2680f6397b
Pandu1712 <prasannaofficial1712@gmail.com>
2025-03-07 14:30:06 +0530

    removed 'a' tags and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Hero from '../app/Hero/page'
 import Education from '../app/Education/page'
 import Projects from '../app/Projects/page'
 
-import Footer from '../app/Footer/page'
 import Skills from '../app/Skills/page'
 import Contact from '../app/Contact/page'
 const page = () => {
